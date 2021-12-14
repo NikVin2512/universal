41310ec6230553af4687c506fc8ff203064a5a8c
Nikolay Vinogradov <nikolay.vin93@gmail.com>
2021-12-14 23:45:48 +0300

    create: added tabs block useful

--- js/main.js
+++ js/main.js
@@ -3,4 +3,14 @@ $(document).ready(function(){
     menuBurger.addEventListener("click", function(){
     document.querySelector('.navbar').classList.toggle('navbar--visible');
   }); 
+  var usefulItem = $(".useful-articles__item");
+  var usefulPreview = $(".useful-articles__preview");
+
+  usefulItem.on("click", function (event) {
+    var activePreview = $(this).attr("data-target");
+    usefulItem.removeClass("useful-articles__item--active");
+    usefulPreview.removeClass("useful-articles__preview--active");
+    $(activePreview).addClass("useful-articles__preview--active");
+    $(this).addClass("useful-articles__item--active");
+  })
 });
