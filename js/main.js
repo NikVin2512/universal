a543f9a96c9d6a140c47411ff3acd74272e6e637
Nikolay Vinogradov <nikolay.vin93@gmail.com>
2021-12-21 00:44:09 +0300

    create: added modal window

--- js/main.js
+++ js/main.js
@@ -46,153 +46,64 @@ $(document).ready(function(){
       }
     });
   });
-  
+var modalButton = $('[data-toggle=modal]');
+var modalClose = $('.modal__close');
+  modalButton.on('click', openModal);
+  modalClose.on('click', closeModal);
+
+function openModal() {
+  var modalWindow = $('.modal');
+  var modalBg = $('.modal-bg');
+  modalWindow.addClass('modal--active');
+  modalBg.addClass('modal-bg--active');
+}
+
+function closeModal(event) {
+  var modalWindow = $('.modal');
+  var modalBg = $('.modal-bg');
+  modalWindow.removeClass('modal--active');
+  modalBg.removeClass('modal-bg--active');
+  event.preventDefault();
+}
+$(document).on('keydown',function(event) {
+  if (event.keyCode == 27) {
+      closeModal();
+   }
+  });
     $(".news-item__favorite").click(function() {
       $(this).toggleClass("news-item__favorite--active");
     });
     $(".slider-head__bookmark").click(function() {
       $(this).toggleClass("slider-head__bookmark--active");
     });
-  document.getElementById("more").onclick = () => {
-      document.getElementById("comments").innerHTML += `<div class="comments__middle-item">
-      <img
-        src="img/info-avatar.png"
-        alt="Аватар"
-        class="comments__avatar"
-      />
-      <div class="comments__content">
-        <div class="comments__content-title">
-          <div class="comments__content-name">Михаил Степашин</div>
-          <div class="comments__content-date">
-            <span class="comments__day">17</span
-            ><span class="comments__month">июля,</span
-            ><span class="comments__time">18:38</span>
-          </div>
-        </div>
-        <div class="comments__content-text">
-          Хочу заметить, что автор этой статьи не написал, что не все
-          могут получить статус самозанятого. Этот режим еще не ввели во
-          всех регионах. Но если в вашем регионе есть, то поздравляю.
-        </div>
-        <div class="comments__content-nav">
-          <div class="comments__content-answer">
-          <a href="#answer"
-          ><img src="img/score.svg" alt="Ответ" /> Ответить</a
-        >
-          </div>
-          <div class="comments__content-block">
-            <img
-              src="img/up.png"
-              alt="Нравится"
-              class="comments__content-like"
-            />
-            <span
-              class="
-                comments__content-score comments__content-score--green
-              "
-              >+10</span
-            >
-            <img
-              src="img/down.png"
-              alt="Не нравится"
-              class="comments__content-dis"
-            />
-          </div>
-        </div>
-      </div>
-    </div><div class="comments__middle-item">
-    <img
-      src="img/avatar.png"
-      alt="Аватар"
-      class="comments__avatar"
-    />
-    <div class="comments__content">
-      <div class="comments__content-title">
-        <div class="comments__content-name">Михаил Степашин</div>
-        <div class="comments__content-date">
-          <span class="comments__day">17</span
-          ><span class="comments__month">июля,</span
-          ><span class="comments__time">18:38</span>
-        </div>
-      </div>
-      <div class="comments__content-text">
-        Хочу заметить, что автор этой статьи не написал, что не все
-        могут получить статус самозанятого. Этот режим еще не ввели во
-        всех регионах. Но если в вашем регионе есть, то поздравляю.
-      </div>
-      <div class="comments__content-nav">
-        <div class="comments__content-answer">
-        <a href="#answer"
-        ><img src="img/score.svg" alt="Ответ" /> Ответить</a
-      >
-        </div>
-        <div class="comments__content-block">
-          <img
-            src="img/up.png"
-            alt="Нравится"
-            class="comments__content-like"
-          />
-          <span
-            class="
-              comments__content-score comments__content-score--green
-            "
-            >+10</span
-          >
-          <img
-            src="img/down.png"
-            alt="Не нравится"
-            class="comments__content-dis"
-          />
-        </div>
-      </div>
-    </div>
-  </div><div class="comments__middle-item">
-  <img
-    src="img/info-avatar.png"
-    alt="Аватар"
-    class="comments__avatar"
-  />
-  <div class="comments__content">
-    <div class="comments__content-title">
-      <div class="comments__content-name">Михаил Степашин</div>
-      <div class="comments__content-date">
-        <span class="comments__day">17</span
-        ><span class="comments__month">июля,</span
-        ><span class="comments__time">18:38</span>
-      </div>
-    </div>
-    <div class="comments__content-text">
-      Хочу заметить, что автор этой статьи не написал, что не все
-      могут получить статус самозанятого. Этот режим еще не ввели во
-      всех регионах. Но если в вашем регионе есть, то поздравляю.
-    </div>
-    <div class="comments__content-nav">
-      <div class="comments__content-answer">
-      <a href="#answer"
-      ><img src="img/score.svg" alt="Ответ" /> Ответить</a
-    >
-      </div>
-      <div class="comments__content-block">
-        <img
-          src="img/up.png"
-          alt="Нравится"
-          class="comments__content-like"
-        />
-        <span
-          class="
-            comments__content-score comments__content-score--green
-          "
-          >+10</span
-        >
-        <img
-          src="img/down.png"
-          alt="Не нравится"
-          class="comments__content-dis"
-        />
-      </div>
-    </div>
-  </div>
-  </div>`;
-      return false;
-  };
+    $(".form").each(function(){
+      $(this).validate({
+        errorClass: 'invalid',
+        rules: {
+          name: {
+            required: true,
+            minlength: 3,
+          },
+          email: {
+            required: true,
+            email: true
+          },
+          phone: {
+            required: true,
+            minlength: '17',
+          },
+        },
+        messages: {
+          name: "Минимум 3 символа ",
+          phone: "Ваш номер телефона в виде +7(999) 999-99-99",
+          email: {
+            required: "Укажите свой почтовый адрес для связи с вами",
+            email: "Неверный формат имя@домен.ру"
+          }
+        }
+      });
+    });
+    $(document).ready(function(){
+      $('.phone-us').mask('+7(000) 000-00-00');
+    });
 });
